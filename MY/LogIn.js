c139a75ce25a09349364da0c54f9d38fe848ab09
vedant-2506 <swamidivate2506@mail.com>
2025-10-11 00:27:16 +0530

    Log IN Page JS file

---
+++ MY/LogIn.js
@@ -0,0 +1,29 @@
+// login.js
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+  // Form validation
+  const loginForm = document.getElementById("loginForm");
+
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const userId = document.getElementById("userId").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (userId === "" || password === "") {
+      alert(" Please fill in all fields.");
+      return;
+    }
+
+    // Temporary demo validation
+    if (userId === "admin" && password === "12345") {
+      alert("Login Successful! Redirecting...");
+    //   window.location.href = "dashboard.html"; // Example redirect
+    } else {
+      alert("Invalid User ID or Password!");
+    }
+  });
+});
